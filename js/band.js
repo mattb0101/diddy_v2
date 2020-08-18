4f9bea86b5619ecaf240a1979792462988028898
mattb0101 <mattbaum666@googlemail.com>
2020-08-18 20:05:30 +0000

    Adding in JavaScript for band page where i didnt kknow it before - click events

---
+++ js/band.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    $(".band-member.dave").on("click", function(){
+        $(".member-name").html("Dave <a href='https://www.instagram.com/gigs_and_games/' target='_blank'><i class='fab fa-instagram'></i></a>");
+    });
+    $(".band-member.will").on("click", function(){
+        $(".member-name").html("Will");
+    });
+    $(".band-member.ryan").on("click", function(){
+        $(".member-name").html("Ryan");
+    });
+    $(".band-member.matt-s").on("click", function(){
+        $(".member-name").html("Matt");
+    });
+    $(".band-member.belle").on("click", function(){
+        $(".member-name").html("Belle");
+    });
+    $(".band-member.gary").on("click", function(){
+        $(".member-name").html("Gary");
+    });
+    $(".band-member.megan").on("click", function(){
+        $(".member-name").html("Megan");
+    });
+    $(".band-member.laurie").on("click", function(){
+        $(".member-name").html("Laurie");
+    });
+    $(".band-member.matt-b").on("click", function(){
+        $(".member-name").html("Matt");
+    });
+
+});
